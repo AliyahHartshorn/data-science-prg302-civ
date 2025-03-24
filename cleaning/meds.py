fe42b75b32092c7809ea7d5ae367dfdd0be26a55
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2025-03-24 15:24:07 +0800

    clean down to 4 options

--- cleaning/meds.py
+++ cleaning/meds.py
@@ -1,13 +1,27 @@
 import pandas
+from re import search
 
 df=pandas.read_csv("cleaning\\medical_students_dataset.csv")
 
 def map_gender(gender_str):
-    first=str(gender_str)[0].lower()
+    # lookup={"non-binary":"Nonbinary",
+    #         "nonbinary":"Nonbinary",
+    #         'nb':"Nonbinary",
+    #         "n":"Nonbinary",
+    #         "o":"Unspecified",
+    #         "other":"Unspecified",
+    #         "nale":"Male"}
+    lwr=str(gender_str).lower()
+    first=lwr[0]
+    if (search("fem",lwr)):
+        return "Female"
     if (first=="m"):
         return "Male"
     elif (first=="f"):
         return "Female"
     else:
         return gender_str
+        # tx=lookup.get(str(gender_str).lower())
+        # return gender_str if tx is None else tx
+lookup=pandas.DataFrame()
 print(df["Gender"].apply(map_gender).unique())
