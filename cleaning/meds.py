67dba87b11b1f08a94a5fc08cbec6134c1e8a092
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2025-03-24 15:17:30 +0800

    exercises

--- cleaning/meds.py
+++ cleaning/meds.py
@@ -1,4 +1,13 @@
 import pandas
 
 df=pandas.read_csv("cleaning\\medical_students_dataset.csv")
-print(df)
+
+def map_gender(gender_str):
+    first=str(gender_str)[0].lower()
+    if (first=="m"):
+        return "Male"
+    elif (first=="f"):
+        return "Female"
+    else:
+        return gender_str
+print(df["Gender"].apply(map_gender).unique())
