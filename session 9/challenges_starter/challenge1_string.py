570d279778c550bb20629ff17c5f4095f245a6ef
Chris Arnold <chris.arnold@nmtafe.wa.edu.au>
2025-04-02 09:39:10 +0800

    revert to starter code

--- session 9/challenges_starter/challenge1_string.py
+++ session 9/challenges_starter/challenge1_string.py
@@ -1,25 +1,9 @@
-# # Open the file in read mode
-# file_path = "data.txt"
-# with open(file_path, "r") as file:
-#     content = file.read()
-content = "Python is a versatile programming language. It is widely used for web development and data analysis."
+# Open the file in read mode
 
 # Find the positions of the substring
-start_marker = "versatile programming language."
-end_marker = "It is widely used for"
-start_index = content.find(start_marker)
-end_index = content.find(end_marker) + len(end_marker)
 
 # Extract the substring using the found positions
-substring = content[start_index:end_index]
 
 # Replace occurrences of a word in the substring
-word_to_replace = "is"
-replacement_word = "was"
-modified_substring = substring.replace(word_to_replace, replacement_word)
 
 # Print the extracted and modified substrings
-print("Extracted Substring:")
-print(substring)
-print("\nModified Substring:")
-print(modified_substring)
--- session 9/challenges_starter/challenge2_read_file.py
+++ session 9/challenges_starter/challenge2_read_file.py
@@ -1,18 +1,7 @@
 # Open the file in read mode
-file_path = "sample.txt"
-with open(file_path, "r") as file:
+
     # Create variables for the count
-    total_lines = 0
-    total_words = 0
 
     # Loop through each line in the file
-    for line in file:
-        total_lines += 1
-
         # Split the line into a list of words and count them
-        words = line.split()
-        total_words += len(words)
-
     # Print the results
-    print(f"Total lines in the file: {total_lines}")
-    print(f"Total words in the file: {total_words}")
