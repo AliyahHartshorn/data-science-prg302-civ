5d064778a5b882f434514e225f000e7916a81c0a
Chris Arnold <Chris.Arnold@nmtafe.wa.edu.au>
2025-05-12 15:55:25 +0800

    abstract class example

---
+++ session 13/oo_ex1.py
@@ -0,0 +1,15 @@
+import wordlist
+import os
+
+def createReader(filename):
+    file_name,file_ext=os.path.splitext(filename)
+    if (file_ext==".json"):
+        return wordlist.JsonFileWordList(filename)
+    else:
+        return wordlist.CsvFileWordList(filename)
+    
+textwords=createReader("..\\session 11\\target_words.txt")
+jsonwords=createReader("words.json")
+
+assert textwords.has_word("abled")
+assert jsonwords.has_word("abled")
---
+++ session 13/wordlist.py
@@ -0,0 +1,69 @@
+import json
+import abc
+
+class TextFileWordList(metaclass=abc.ABCMeta):
+    """Here's an example of an abstract class. It contains
+    the relevant API (it's very simple, just the add_word
+    and has_word methods) and the relevant behaviour
+    for loading a text file."""
+    def __init__(self,filename):
+        with open(filename,'r') as words_file:
+            self.words=self._decode_text_file(words_file)
+    
+    @abc.abstractmethod
+    def _decode_text_file(self,words_file):
+        """
+        This method is the reason we have an abstract class.
+        We want "is a" relationships (see the demo code in 
+        week 12), and there's no "is a" relationship between the 
+        JSON and CSV types, they're just different ways of
+        decoding files. Instead, we have a decode method that's
+        marked abstract. This means the decode logic must be
+        provided, but it's not provided here.
+        """
+        pass
+
+    def has_word(self,word):
+        return word in self.words 
+
+    def add_word(self,word):
+        self.words.append(word)
+    
+    def save_list(self,filename):
+        """Not implemented"""
+        pass
+    
+class CsvFileWordList(TextFileWordList):
+    """This subclass has decoding logic for the simple files
+    we were give: one word per line. It's not really a full-
+    blown CSV decoder"""
+    def _decode_text_file(self,words_file):
+        result=words_file.readlines()
+        return map(lambda x:x.strip(),result)
+
+class JsonFileWordList(TextFileWordList):
+    """This subclass has decoding logic for json files"""
+    def _decode_text_file(self,words_file):
+        json_data=words_file.read()
+        json_obj=json.loads(json_data)
+        return json_obj["words"]
+
+class VariableJsonList(JsonFileWordList):
+    """This is an example extension. The model is starting
+    to get flimsy here! So the class structure needs rework"""
+    six_letter_words=[]
+    seven_letter_words=[]
+    def _decode_text_file(self,words_file):
+        json_data=words_file.read()
+        json_obj=json.loads(json_data)
+        self.six_letter_words=json_obj["six letter words"]
+        return json_obj["words"]
+        
+    def has_word(self,word):
+        if len(word)==5:
+            return super().has_word(word)
+        
+        elif len(word)==6:
+            return word in self.six_letter_words
+        elif len(word)==7:
+            return word in self.seven_letter_words 
